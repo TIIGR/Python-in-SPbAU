3dd51b10682b6ee1eea411906aa3d5fca6244a56
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-07-23 20:08:58 +0300

    +

--- TextCoding/textcoding.py
+++ TextCoding/textcoding.py
@@ -2,16 +2,18 @@
 
 def de_en_code(text):
     code = ''
-    if chr(1425) == text[0]:
+    if len(text) == 0:
+        return print('Текстовый файл пуст!')
+    elif ord(text[0]) == 1425:
         for Ord in range(1, len(text)):
             if ord(text[Ord]) < 1424:
-                code = code + chr((ord(text[Ord]) + ((-1) ** (Ord)) * Ord) % 1424)
+                code += chr((ord(text[Ord]) + ((-1) ** Ord) * Ord) % 1424)
             else:
                 return print('Присутствуют недопустимые символы! Один из таких:', text[Ord])
     else:
         for Ord in range(0, len(text)):
             if ord(text[Ord]) < 1424:
-                code = code + chr((ord(text[Ord]) + ((-1) ** Ord) * (Ord + 1)) % 1424)
+                code += chr((ord(text[Ord]) + ((-1) ** Ord) * (Ord + 1)) % 1424)
             else:
                 return print('Присутствуют недопустимые символы! Один из таких:', text[Ord])
         code = chr(1425) + code
