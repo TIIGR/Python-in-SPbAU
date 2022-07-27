2c162f8cf186dec0fa1c51f93cb8d20aeee67fa0
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-07-28 00:09:25 +0300

    +

--- prime_func.py
+++ prime_func.py
@@ -14,15 +14,15 @@ def Pf(x, eps):
         p += 1
     return taylor
 
-def desmos(x_min, x_max, step, eps):
-    if (step <= 0 or eps <= 0):
+def desmos(x_min, x_max, eps):
+    if (eps <= 0):
         print('Недопустимые вводные данные')
         return -1
     X = []; Y = []; x_p = x_min
     while (x_min <= x_p <= x_max):
         X.append(x_p)
         Y.append(Pf(x_p, eps))
-        x_p += step
+        x_p += (x_max - x_min) / 1000
     plt.plot(X, Y)
     plt.title('"Простая" функа')
     plt.show()
@@ -31,7 +31,6 @@ def desmos(x_min, x_max, step, eps):
 try:
     desmos(float(input('Начало локальной области определения функи: ')),
         float(input('Конец локальной области определения функи: ')),
-        float(input('Шаг построения точек функи: ')),
         float(input('Погрешность значения функи: ')))
 except ValueError:
     print('Недопустимые вводные данные (ValueError)')
