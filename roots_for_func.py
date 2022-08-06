d9e2d490e7507d194440b1ae1b315839866fd4c1
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-08-06 22:07:32 +0300

    +

---
+++ roots_for_func.py
@@ -0,0 +1,40 @@
+from math import *
+from math import pi, e
+from sympy import diff
+from numpy import sign
+
+
+def roots_for_func(func, x_min, x_max):
+    if x_max <= x_min:
+        print('Недопустимые вводные данные')
+        return -1
+    ROOTS = []; x = x_min
+    dfunc = str(diff(func, 'x'))
+    while (x_min <= x <= x_max):
+        x_p = x + (x_max - x_min) / 10 ** 3
+        if (eval(func) * eval(func.replace('x','x_p'))) / (eval(dfunc) * eval(dfunc.replace('x','x_p'))) <= 0:
+            x_0 = x
+            x_1 = x - (eval(func) / eval(dfunc))
+            while (abs(x_0 - x_1) >= 10 ** -11 and (x_min <= x_1 <= x_max)):
+                x_0 = x_1
+                x_1 -= eval(func.replace('x','x_1')) / eval(dfunc.replace('x','x_1'))
+            if x_min <= x_1 <= x_max:
+                ROOTS.append(sign(round(x_1))*abs(round(x_1, 5)))
+        x += x_p - x
+    if ROOTS != []:
+        print('Для F(x) =', func, 'при x ∈ [' + str(x_min) + ', ' + str(x_max) + '] обнаружены корни:', ROOTS)
+    else:
+        print('Корни не обнаружены на заданом локальном области определения в вещественном поле чисел!')
+    return 0
+
+
+try:
+    roots_for_func(str(input('Введите функу F(x): ')),
+    float(input('Начало локальной области определения функи: ')),
+    float(input('Конец локальной области определения функи: ')))
+except ValueError:
+    print('Некорректно введенные данные ...')
+except TypeError:
+    print('Некорректно введенные данные ...')
+except NameError:
+    print('Поддерживается только следующие функции и константы: pi, e, стандартные функции ...')
