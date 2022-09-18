3d21212c2f6eb490908a266b0609b0c0f7d409ad
♫♣MΩTΩRΩLѦ♣♫ <55460474+TIIGR@users.noreply.github.com>
2022-09-18 03:53:59 +0300

    update

--- textcoding.py
+++ textcoding.py
@@ -4,19 +4,19 @@ def de_en_code(text):
     code = ''
     if len(text) == 0:
         return print('Текстовый файл пуст!')
-    elif ord(text[0]) == 1425:
+    elif ord(text[0]) == 1423:
         for Ord in range(1, len(text)):
-            if ord(text[Ord]) < 1424:
-                code += chr((ord(text[Ord]) + ((-1) ** Ord) * Ord) % 1424)
+            if ord(text[Ord]) < 1423:
+                code += chr((ord(text[Ord]) + ((-1) ** Ord) * Ord) % 1423)
             else:
                 return print('Присутствуют недопустимые символы! Один из таких:', text[Ord])
     else:
         for Ord in range(0, len(text)):
-            if ord(text[Ord]) < 1424:
-                code += chr((ord(text[Ord]) + ((-1) ** Ord) * (Ord + 1)) % 1424)
+            if ord(text[Ord]) < 1423:
+                code += chr((ord(text[Ord]) + ((-1) ** Ord) * (Ord + 1)) % 1423)
             else:
                 return print('Присутствуют недопустимые символы! Один из таких:', text[Ord])
-        code = chr(1425) + code
+        code = chr(1423) + code
     open(Directory, 'w+', encoding='utf-8').write(code)
     if len(text) < len(code):
         return print('Текст зашифрован!')
